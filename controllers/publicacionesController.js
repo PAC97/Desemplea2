679fa37f98240010e893c2aa0db91d7ab67f3138
Pedro Cano <pedrinche27@gmail.com>
2019-10-08 14:26:56 -0600

    Publicaciones retornar objeto de Servicio

--- controllers/publicacionesController.js
+++ controllers/publicacionesController.js
@@ -28,15 +28,18 @@ exports.crearPublicacion = async (req, res) => {
 exports.publicacionPorId = async (req, res, next) => {
     const id = req.params.id;
     
-   const publicacion = await Publicaciones.findById(id, (err, publicacion)=>{   
-        if(err) res.status(500).json({mensaje: `Error al realizar la peticion: ${err}`})
-        if(!publicacion) res.status(404).json({mensaje: 'No se encuentra ese dato'})
-        else{
-            res.status(200).json({status: 'Success', publicacion: publicacion})
-            console.log(publicacion);
-        }
+   const publicacion = await Publicaciones.findById({_id: id}).
+   populate('ID_Servicio').
+   exec((err, publicacion)=>{
+    if(err) res.status(500).json({mensaje: `Error al realizar la peticion: ${err}`})
+    if(!publicacion) res.status(404).json({mensaje: 'No se encuentra ese dato'})
+    else{
+        res.status(200).json({status: 'Success', publicacion: publicacion})
+        console.log(publicacion);
+    }
+   }) 
         
-    })
+    
 }
 
 exports.actualizarPublicacion = async (req, res) => {
@@ -70,7 +73,9 @@ exports.eliminarPublicacion = async (req, res) => {
 exports.publicacionPorUsuario = async (req, res)=>{
     const idUsuario = req.params.usuario;
     console.log(idUsuario);
-    const publicacion = await Publicaciones.find({$or:[{'Usuario':idUsuario}]},(err, publicaciones)=>{
+    const publicacion = await Publicaciones.find({$or:[{'Usuario':idUsuario}]}).
+    populate('ID_Servicio').
+    exec((err, publicaciones)=>{
         if(err){
             throw err;
         }
