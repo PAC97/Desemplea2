55a99aeab1e447c798d35c26111329a54470150c
Pedro Cano <pedrinche27@gmail.com>
2019-11-04 11:17:10 -0600

    Chat controller

---
+++ controllers/chatController.js
@@ -0,0 +1,13 @@
+const Chat = require('../models/Chat');
+
+exports.ChatLista = async (req, res) => {
+    await Chat.find({Emisor : req.params.id})
+    .populate('Emisor')
+    .exec((err, Chat) => {
+      if(err) res.status(500).json({mensaje: 'Ocurrio un error'})
+      if(!Chat){
+        res.status(403).json({mensaje: 'Lista Vacia'})
+      }
+      res.status(200).json({status: 'Success', Chat: Chat})
+    })
+  }
